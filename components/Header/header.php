70513de43a14cf7a3fd4ea1269c4d0668047f67e
Rakib Ahmed <rakib987@gmail.com>
2021-12-12 02:56:17 -0500

    relink

--- components/Header/header.php
+++ components/Header/header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <nav class="nav-menu navigation" id="navigation">
-        <li class="nav-link Home"><a href="/" title="Home"><i class="fas fa-home"></i> MyDex | PokeDex Clone</a></li>
+        <li class="nav-link Home"><a href="./" title="Home"><i class="fas fa-home"></i> MyDex | PokeDex Clone</a></li>
         <div class="social-links" id="socialLinksMain">
             <div class="mainNavLink">
                 <a href="https://github.com/strawhat19/" class="nav-link githubLink" title="Github Repo" target="_blank">
@@ -10,10 +10,10 @@
             <div class="mainNavLink">- Welcome, Login to Save Pokemon!</div>
         </div>
         <div class="registrationButtons">
-            <a href="/about.php" title="About">
+            <a href="./about.php" title="About">
                 <i class="fas fa-sign-in-alt"></i> About
             </a>
-            <a href="/profile.php" title="Profile">
+            <a href="./profile.php" title="Profile">
                 <i class="fas fa-user-plus"></i> Profile
             </a>
         </div>
@@ -26,9 +26,9 @@
                 </a>
             </div>
             <ul class="desktop-menu-option mobile-Menu" id="desktopMenu">
-                <li class="nav-link Github"><a href="https://github.com/strawhat19/Whats-Your-Deal"><i class="fab fa-github"></i> Github</a></li>
-                <li class="nav-link"><a href="/about.php" title="About"><i class="fas fa-sign-in-alt"></i> About</a></li>
-                <li class="nav-link"><a href="/profile.php" title="Profile"><i class="fas fa-user-plus"></i> Profile</a></li>
+                <li class="nav-link Github"><a href="https://github.com/strawhat19/"><i class="fab fa-github"></i> Github</a></li>
+                <li class="nav-link"><a href="./about.php" title="About"><i class="fas fa-sign-in-alt"></i> About</a></li>
+                <li class="nav-link"><a href="./profile.php" title="Profile"><i class="fas fa-user-plus"></i> Profile</a></li>
             </ul>
         </div>
     </nav>
