f44b69eab1656f98378b9bad6658ac66619d86c2
Rakib Ahmed <rakib987@gmail.com>
2021-12-13 21:04:34 -0500

    with dark

--- pokemon.php
+++ pokemon.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://cdn-icons.flaticon.com/png/512/280/premium/280954.png?token=exp=1639231379~hmac=eab3f20aec44610c32813fb3ff34d811" type="image/x-icon">
+    <link rel="icon" href="https://cdn-icons.flaticon.com/png/512/280/premium/280954.png?token=exp=1639447053~hmac=feb97b0684df70dbf7fe09531a88820a" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.css">
     <link rel="stylesheet" href="./assets/styles/index.css" />
     <title>Pokemon | MyDex</title>
