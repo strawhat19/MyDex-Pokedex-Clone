a78beffca4f742e17cf6418a7e6e65ddfe640b91
Rakib Ahmed <rakib987@gmail.com>
2021-12-21 23:06:06 -0500

    new banner

--- components/Search/search.php
+++ components/Search/search.php
@@ -88,7 +88,7 @@ if ($(event.target).val() == ``) {
 </script>
 
 <form class="searchFilter">
-    <div class="formLabel">Search & Filter by Name or Type</div>
+    <div class="formLabel">The Pokedex</div>
     <div class="inputs">
         <input id="pokeSearch" type="text" name="search" placeholder="Search Pokemon by Name..." />
         <span class="pokeNum"></span>
